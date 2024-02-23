3166738756d4c01e90224452e09140d3cac0ec72
Breakout-X <153556885+Breakout-X@users.noreply.github.com>
2024-02-23 16:47:02 -0500

    Update ContentView.swift

--- archive/Code/ContentView.swift
+++ archive/Code/ContentView.swift
@@ -3,6 +3,7 @@ import SwiftUI
 //this ContentView loads a local resource
 struct ContentView: View {
     var body: some View {
-        WebView(request: URLRequest(url: Bundle.main.url(forResource: "index", withExtension: "html")!))
+        let url = Bundle.main.url(forResource: "index", withExtension: "html")!
+        WebView(request: URLRequest(url: url), baseURL: url.deletingLastPathComponent())
     }
 }
