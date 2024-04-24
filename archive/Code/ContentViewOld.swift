8aab603cb0cd1b5f61fda1749afb922464089013
Breakout-X <153556885+Breakout-X@users.noreply.github.com>
2024-04-24 19:34:17 -0400

    Update and rename ContentView.swift to ContentViewOld.swift

--- archive/Code/ContentViewOld.swift
+++ archive/Code/ContentViewOld.swift
@@ -1,7 +1,7 @@
 import SwiftUI
 
 //this ContentView loads a local resource
-struct ContentView: View {
+struct ContentViewOld: View {
     var body: some View {
         let url = Bundle.main.url(forResource: "index", withExtension: "html")!
         WebView(request: URLRequest(url: url), baseURL: url.deletingLastPathComponent())
